2c42360bfa7e5e9fa057e3451ee7a4102a442d75
Maria Fernanda Oliveira <mariafernandasso281@gmail.com>
2023-01-29 19:02:06 -0300

    Finalizado o projeto spring data jpa com integração do banco mysql

--- dio-spring-boot/aula-spring-data-jpa/src/main/java/dio/aula/StartApp.java
+++ dio-spring-boot/aula-spring-data-jpa/src/main/java/dio/aula/StartApp.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 import dio.aula.model.User;
 import dio.aula.repository.UserRepository;
 
+import java.util.List;
+
 @Component
 public class StartApp implements CommandLineRunner{
 
@@ -15,12 +17,19 @@ public class StartApp implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
+        List<User> users = repository.filtrarPorNome("Maria Fernanda");
+        for (User u: users) {
+            System.out.println(u);
+        }
+
+        insertUser();
+    }
+
+    private void insertUser(){
         User user = new User();
         user.setName("Maria Fernanda");
-        user.setUsername("Mariazinha123");
-        user.setPassword("maria123");
-        
-        
+        user.setUsername("Mariazinha124");
+        user.setPassword("maria124");
         repository.save(user);
 
         for (User u : repository.findAll()) {
