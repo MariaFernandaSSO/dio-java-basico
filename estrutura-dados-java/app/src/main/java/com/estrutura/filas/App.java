b4f653c614ffce0853d60d82a754f0c8615a5294
Maria Fernanda Oliveira <mariafernandasso281@gmail.com>
2023-03-08 14:47:44 -0300

    Estudo da estrutura de dados Lista Encadeada em java

---
+++ estrutura-dados-java/app/src/main/java/com/estrutura/filas/App.java
@@ -0,0 +1,22 @@
+package com.estrutura.filas;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("---------- Fila -----------");
+
+        Fila<String> minhaFila = new Fila<>();
+        
+        minhaFila.enqueue("primeiro");
+        minhaFila.enqueue("segundo");
+        minhaFila.enqueue("terceiro");
+        minhaFila.enqueue("quarto");
+
+        System.out.println(minhaFila);
+        System.out.println(minhaFila.dequeue());
+        System.out.println(minhaFila);
+        minhaFila.enqueue("ultimo");
+        System.out.println(minhaFila);
+        System.out.println(minhaFila.first());
+        System.out.println(minhaFila);
+    }
+}
