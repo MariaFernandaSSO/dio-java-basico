ddeb5406b4a907b9c87bc17a70e64a8b406b2bfc
Maria Fernanda Oliveira <mariafernandasso281@gmail.com>
2024-01-19 09:33:30 -0300

    teste: alteração de arquivo e estudo

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,6 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+
+        Scanner scan = new Scanner(System.in);
+
+        Random random = new Random(); // gerar números aleatórios
+
+        int[] numeros = new int[15];
+
+        for (int i = 0; i < numeros.length; i++) {
+            int numero = random.nextInt(100);
+            numeros[i] = numero;
+        }
+
+        System.out.print("Números Aleatórios: ");
+        for (int numero : numeros) {
+            System.out.print(numero + " ");
+        }
+
+        System.out.print("\nSucessores dos Números Aleatórios: ");
+        for (int numero : numeros) {
+            System.out.print((numero + 1) + " ");
+        }
+
+        System.out.print("\nAntecessores dos Números Aleatórios: ");
+        for (int numero : numeros) {
+            System.out.print((numero - 1) + " ");
+        }
+
+        System.out.println("\nsocorro deus sou antisocial");
     }
 }
